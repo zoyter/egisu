bef7560cae3a1014fa3b9c7d5c83a351979ce03c
Oleg <zoyter@users.noreply.github.com>
2023-08-30 23:00:30 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,45 @@
+import json
+import requests
+import  os
+
+ext='.json'
+base_url='https://rosrid.ru/api/open-data?'
+
+card_types=[
+    ('Сведения о результатах научно-исследовательских работ','ikrbs'),
+    ('Сведения о защищённых диссертациях на соискание учёных степеней','dissertation'),
+    ('Сведения о результате интеллектуальной деятельности','rid'),
+    ('Сведения об использовании РИД','iksi'),
+    ('Состояния правовой охраны результатов интеллектуальной деятельности','ikspo'),
+]
+
+def get_data(tables=None,year_start=2016,year_stop=2022):
+    print(f'Скачивание открытых данных из ЕГСУ НИОКТР')
+    for card_type in tables:
+        print(f'\t - {card_type[0]}:')
+        for year in range(year_start,year_stop+1):
+            folder = f'{year}'
+            print(f'\t\t - {folder} год: ', end='')
+            if not os.path.isdir(folder):
+                os.mkdir(folder)
+            for month in range(1,12+1):
+                m=f'{month:02}'
+                print(f'{m}',end=',')
+                url=f'{base_url}year={year}&month={m}&card_type={card_type[1]}'
+                r = requests.get(url)
+                data = r.json()
+                filename = os.path.join(folder,m+'.json')
+                with open(filename,'w', encoding='utf-8') as f:
+                    json.dump(data,f,ensure_ascii=False)
+            print()
+    print('Скачивание завершено')
+
+# За всё время
+# get_data(tables=card_types)
+
+# Только за 2022 год
+get_data(tables=card_types,year_start=2022,year_stop=2022)
+
+# за период 2020-2022
+# get_data(tables=card_types,year_start=2021,year_stop=2022)
+
